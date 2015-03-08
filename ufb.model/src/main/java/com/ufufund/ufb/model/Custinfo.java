12984ee63693a3639e79d079e1f42e6fdbff366e
ayis <nonopo12345@163.com>
2015-03-08 13:16:23 +0800

    custinfo表dto

--- ufb.model/src/main/java/com/ufufund/ufb/model/Custinfo.java
+++ ufb.model/src/main/java/com/ufufund/ufb/model/Custinfo.java
@@ -2,43 +2,174 @@ package com.ufufund.ufb.model;
 
 import java.io.Serializable;
 
+/**
+ * 用户信息表dto
+ * @author ayis
+ *
+ */
 public class Custinfo implements Serializable{
-	private static final long serialVersionUID = 2823000821868950247L;
+	private static final long serialVersionUID = -6338092203128112153L;
 
-	private String custNo;
+	private String custno;
 	
-	private String custType;
+	private String invtp;
 	
-	private String areaId;
+	private String intnm;
+	
+	private String sex;
+	
+	private String idtype;
+	
+	private String idno;
+	
+	private String mobileno;
+	
+	private String email;
+	
+	private String custst;
+	
+	private String passwd;
+	
+	private String lastlogintime;
+	
+	private String passwderr;
+	
+	private String opendt;
+	
+	private String tradepwd;
+	
+	private String updatetimestamp;
 
-	public String getCustNo() {
-		return custNo;
+	public String getCustno() {
+		return custno;
 	}
 
-	public void setCustNo(String custNo) {
-		this.custNo = custNo;
+	public void setCustno(String custno) {
+		this.custno = custno;
 	}
 
-	public String getCustType() {
-		return custType;
+	public String getInvtp() {
+		return invtp;
 	}
 
-	public void setCustType(String custType) {
-		this.custType = custType;
+	public void setInvtp(String invtp) {
+		this.invtp = invtp;
 	}
 
-	public String getAreaId() {
-		return areaId;
+	public String getIntnm() {
+		return intnm;
 	}
 
-	public void setAreaId(String areaId) {
-		this.areaId = areaId;
+	public void setIntnm(String intnm) {
+		this.intnm = intnm;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getIdtype() {
+		return idtype;
+	}
+
+	public void setIdtype(String idtype) {
+		this.idtype = idtype;
+	}
+
+	public String getIdno() {
+		return idno;
+	}
+
+	public void setIdno(String idno) {
+		this.idno = idno;
+	}
+
+	public String getMobileno() {
+		return mobileno;
+	}
+
+	public void setMobileno(String mobileno) {
+		this.mobileno = mobileno;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCustst() {
+		return custst;
+	}
+
+	public void setCustst(String custst) {
+		this.custst = custst;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+	public String getLastlogintime() {
+		return lastlogintime;
+	}
+
+	public void setLastlogintime(String lastlogintime) {
+		this.lastlogintime = lastlogintime;
+	}
+
+	public String getPasswderr() {
+		return passwderr;
+	}
+
+	public void setPasswderr(String passwderr) {
+		this.passwderr = passwderr;
+	}
+
+	public String getOpendt() {
+		return opendt;
+	}
+
+	public void setOpendt(String opendt) {
+		this.opendt = opendt;
+	}
+
+	public String getTradepwd() {
+		return tradepwd;
+	}
+
+	public void setTradepwd(String tradepwd) {
+		this.tradepwd = tradepwd;
+	}
+
+	public String getUpdatetimestamp() {
+		return updatetimestamp;
+	}
+
+	public void setUpdatetimestamp(String updatetimestamp) {
+		this.updatetimestamp = updatetimestamp;
 	}
 
 	@Override
 	public String toString() {
-		return "Custinfo [custNo=" + custNo + ", custType=" + custType
-				+ ", areaId=" + areaId + "]";
+		return "Custinfo [custno=" + custno + ", invtp=" + invtp + ", intnm="
+				+ intnm + ", sex=" + sex + ", idtype=" + idtype + ", idno="
+				+ idno + ", mobileno=" + mobileno + ", email=" + email
+				+ ", custst=" + custst + ", passwd=" + passwd
+				+ ", lastlogintime=" + lastlogintime + ", passwderr="
+				+ passwderr + ", opendt=" + opendt + ", tradepwd=" + tradepwd
+				+ ", updatetimestamp=" + updatetimestamp + "]";
 	}
+	
 	
 }
