2abc3151d5862838ae399ebe83ec16b4cc2befad
luanchengwei <luan_chengwei@pactera.com>
2015-04-11 13:47:40 +0800

    两个接口 查询交易和份额

--- ufb.remote/src/main/java/com/ufufund/ufb/remote/xml/pojo/BalanceQueryRequest.java
+++ ufb.remote/src/main/java/com/ufufund/ufb/remote/xml/pojo/BalanceQueryRequest.java
@@ -1,5 +1,64 @@
 package com.ufufund.ufb.remote.xml.pojo;
 
-public class BalanceQueryRequest {
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 
+import com.ufufund.ufb.remote.xml.base.AbstractRequest;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class BalanceQueryRequest extends AbstractRequest {
+
+	private String TransactionAccountID;
+	private String FundCode;
+	private String PageNo;
+	private String PageSize;
+	private String ShareClass;
+
+	public String getTransactionAccountID() {
+		return TransactionAccountID;
+	}
+
+	public void setTransactionAccountID(String transactionAccountID) {
+		TransactionAccountID = transactionAccountID;
+	}
+
+	public String getFundCode() {
+		return FundCode;
+	}
+
+	public void setFundCode(String fundCode) {
+		FundCode = fundCode;
+	}
+
+	public String getPageNo() {
+		return PageNo;
+	}
+
+	public void setPageNo(String pageNo) {
+		PageNo = pageNo;
+	}
+
+	public String getPageSize() {
+		return PageSize;
+	}
+
+	public void setPageSize(String pageSize) {
+		PageSize = pageSize;
+	}
+
+	public String getShareClass() {
+		return ShareClass;
+	}
+
+	public void setShareClass(String shareClass) {
+		ShareClass = shareClass;
+	}
+
+	@Override
+	public String toString() {
+		return "TransQueryRequest [TransactionAccountID="
+				+ TransactionAccountID + ", FundCode=" + FundCode + ", PageNo="
+				+ PageNo + ", PageSize=" + PageSize + ", ShareClass="
+				+ ShareClass + ", toString()=" + super.toString() + "]";
+	}
 }
--- ufb.remote/src/test/java/com/ufufund/ufb/remote/test/HftQueryServiceTest.java
+++ ufb.remote/src/test/java/com/ufufund/ufb/remote/test/HftQueryServiceTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.ufufund.ufb.common.constant.Constant;
 import com.ufufund.ufb.remote.service.HftQueryService;
 import com.ufufund.ufb.remote.xml.pojo.BalanceQueryRequest;
 import com.ufufund.ufb.remote.xml.pojo.BalanceQueryResponse;
@@ -15,28 +16,65 @@ import com.ufufund.ufb.remote.xml.pojo.TransQueryRequest;
 import com.ufufund.ufb.remote.xml.pojo.TransQueryResponse;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:spring/spring-ufb-remote.xml"})
+@ContextConfiguration(locations = { "classpath:spring/spring-ufb-remote.xml" })
 public class HftQueryServiceTest {
-	private static Logger LOG = LoggerFactory.getLogger(HftQueryServiceTest.class);
+	private static Logger LOG = LoggerFactory
+			.getLogger(HftQueryServiceTest.class);
 
 	@Autowired
 	private HftQueryService hftQueryService;
-	
+
 	/**
 	 * 交易查询
+	 * 
 	 * @param request
 	 * @return
 	 */
 	@Test
-	public void transQuery(TransQueryRequest request){
+	public void transQuery() {
+		TransQueryRequest request = new TransQueryRequest();
+
+		request.setTransactionAccountID("");
+		request.setAppSheetSerialNo("");
+		request.setPageNo("1");
+		request.setPageSize("10");
+
+		request.setVersion(Constant.HftSysConfig.Version);
+		request.setMerchantId(Constant.HftSysConfig.MerchantId);
+		request.setDistributorCode(Constant.HftSysConfig.DistributorCode);
+		request.setBusinType(Constant.HftBusiType.TransQuery);
+		request.setApplicationNo("20150410CC0001");
+		request.setExtension("");
+
+		TransQueryResponse response = hftQueryService.transQuery(request);
+		LOG.debug("返回对象:" + response.toString());
 	}
-	
+
 	/**
 	 * 份额查询
+	 * 
 	 * @param request
 	 * @return
 	 */
 	@Test
-	public void balanceQuery(BalanceQueryRequest request){
+	public void balanceQuery() {
+		BalanceQueryRequest request = new BalanceQueryRequest();
+		
+		request.setTransactionAccountID("");
+		request.setFundCode("");
+		request.setPageNo("1");
+		request.setPageSize("10");
+		request.setShareClass("10");
+
+		request.setVersion(Constant.HftSysConfig.Version);
+		request.setMerchantId(Constant.HftSysConfig.MerchantId);
+		request.setDistributorCode(Constant.HftSysConfig.DistributorCode);
+		request.setBusinType(Constant.HftBusiType.TransQuery);
+		request.setApplicationNo("20150410CC0001");
+		request.setExtension("");
+
+		BalanceQueryResponse response = hftQueryService.balanceQuery(request);
+		LOG.debug("返回对象:" + response.toString());
+		
 	}
 }
