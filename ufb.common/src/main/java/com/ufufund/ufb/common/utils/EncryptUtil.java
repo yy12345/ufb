e6aa9274041c00b7c79f5ae1fdc313f21ef979ad
ayis <nonopo12345@163.com>
2015-04-14 23:17:40 +0800

    程序生成流水号工具类添加

--- ufb.common/src/main/java/com/ufufund/ufb/common/utils/EncryptUtil.java
+++ ufb.common/src/main/java/com/ufufund/ufb/common/utils/EncryptUtil.java
@@ -1,6 +1,7 @@
 package com.ufufund.ufb.common.utils;
 
 import java.io.UnsupportedEncodingException;
+import java.security.DigestException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -16,9 +17,9 @@ public class EncryptUtil {
      * @return
      */
     public static String md5(String inputText) {  
-        return encrypt("md5", inputText, null);  
+        return digest("md5", inputText, null);  
     }
-    
+	
 	/**
 	 * md5加密   
 	 * @param inputText
@@ -26,8 +27,18 @@ public class EncryptUtil {
 	 * @return
 	 */
     public static String md5(String inputText, String charset) {  
-        return encrypt("md5", inputText, charset);  
+        return digest("md5", inputText, charset);  
     } 
+    
+    /**
+     * md5加密
+     * @param input
+     * @return
+     */
+    public static String md5(byte[] inputBytes){
+    	return digest("md5", inputBytes);
+    }
+    
     
     /**
      * sha加密
@@ -35,7 +46,7 @@ public class EncryptUtil {
      * @return
      */
     public static String sha(String inputText) {  
-        return encrypt("sha-1", inputText, null);  
+        return digest("sha-1", inputText, null);  
     } 
     
     /**
@@ -45,47 +56,58 @@ public class EncryptUtil {
      * @return
      */
     public static String sha(String inputText, String charset) {  
-        return encrypt("sha-1", inputText, charset);  
+        return digest("sha-1", inputText, charset);  
     }  
     
-  
+    /**
+     * sha加密
+     * @param input
+     * @return
+     */
+    public static String sha(byte[] inputBytes){
+    	return digest("sha-1", inputBytes);
+    }
+    
     /** 
      * md5或者sha-1加密 
-     *  
-     * @param inputText 
-     *            要加密的内容 
-     * @param algorithmName 
-     *            加密算法名称：md5或者sha-1，不区分大小写 
+     * @param algorithmName  加密算法名称：md5或者sha-1，不区分大小写 
+     * @param inputText  要加密的文本内容 
+     * @param charset 
      * @return 
      */  
-    private static String encrypt(String algorithmName, String inputText, String charset) {  
-        if (inputText == null) {  
-            throw new IllegalArgumentException("请输入要加密的内容");  
-        }  
-        if (algorithmName == null || "".equals(algorithmName.trim())) {  
-            algorithmName = "md5";  
-        }  
-        String encryptText = null;  
-        try {  
-            MessageDigest m = MessageDigest.getInstance(algorithmName);  
-            byte[] source = null;
+    private static String digest(String algorithmName, String inputText, String charset) {  
+    	String encryptText = null;
+    	try {   
+    		byte[] source = null;
             if(charset == null || "".equals(charset)){
             	source = inputText.getBytes();
             }else{
             	source = inputText.getBytes(charset);
             }
-            byte s[] = m.digest(source);
-//          or ...
-//          m.update(source);  
-//          byte s[] = m.digest();
-            encryptText = hex(s);  
-        } catch (NoSuchAlgorithmException e) {  
-            LOG.error(e.getMessage(), e);
+            encryptText = digest(algorithmName, source);
         } catch (UnsupportedEncodingException e) {  
         	LOG.error(e.getMessage(), e);
         }  
-        return encryptText;  
+        return encryptText;
     }  
+    
+    /**
+     * md5或者sha-1加密 
+     * @param algorithmName 
+     * @param input
+     * @return
+     */
+    private static String digest(String algorithmName, byte[] input){
+    	String encryptText = null;
+    	try {
+			MessageDigest m = MessageDigest.getInstance(algorithmName);
+			byte[] s = m.digest(input);
+			encryptText = hex(s);
+		} catch (NoSuchAlgorithmException e) {
+			LOG.error(e.getMessage(), e);
+		}
+    	return encryptText;
+    }
   
     // 返回十六进制字符串  
     private static String hex(byte[] arr) {  
@@ -95,22 +117,4 @@ public class EncryptUtil {
         }  
         return sb.toString();  
     }  
-    
-    public static void main(String[] args) {  
-    	//String str = "acc_time=20091225091010&bank_buyer_name=测试用户A&bank_card_no=6226095710221222&bank_fund_no=A2013072812000001&bank_kind_no=007&branch_bank_name=招商银行杭州分行&buyer_cert_no=360203197707136088&buyer_cert_type=1&buyer_name=测试用户A&input_charset=GBK&mobile_number=13812345678&partner=1900000107&service_version=1.0&sign_key_index=1&sign_type=MD5&spbill_create_ip=192.168.1.1&transaction_id=OP2013072812000001&user_type=1&key=111111";
-        //md5加密测试  
-//        String md5_1 = md5(str, "gbk");  
-//        String md5_2 = md5(str, "utf-8");  
-//        String md5_3 = md5(str, "iso-8859-1");  
-//        System.out.println(md5_1 + "\n" + md5_2 + "\n" + md5_3);  
-//        System.out.println("md5 length: " + md5_1.length());  
-//        //sha加密测试  
-//        String sha_1 = sha(str, "gbk");  
-//        String sha_2 = sha(str, "utf-8");
-//        String sha_3 = sha(str, "iso-8859-1");
-//        System.out.println(sha_1 + "\n" + sha_2 + "\n" + sha_3);  
-//        System.out.println("sha length: " + sha_1.length());  
-        
-        System.out.println(Integer.toHexString(255));
-    } 
 }
--- ufb.common/src/main/java/com/ufufund/ufb/common/utils/SequenceUtil.java
+++ ufb.common/src/main/java/com/ufufund/ufb/common/utils/SequenceUtil.java
@@ -1,13 +1,174 @@
 package com.ufufund.ufb.common.utils;
 
-public class SequenceUtil {
 
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 程序生成流水号工具类
+ * 备注：
+ * 	1.每秒可并发65535（0xffff）个
+ * 	2.在大量集群的环境下，pid和mac组合串不保证绝对的唯一性；
+ * @author ayis
+ * 2015年4月14日
+ */
+public final class SequenceUtil {
+    private static final Logger LOG = LoggerFactory.getLogger(SequenceUtil.class);
+
+	private static int num = 1;
+	private static String pid = null;
+    private static String mac = null;
+
+    static {
+    	pid = Integer.toHexString((getProcessId() & 0xFF) | 0x100)
+        		.substring(1, 3).toUpperCase();
+        mac = getMachineID(2);
+    }
+	
+    /**
+     * 生成16位长度16进制字符表示的流水号
+     * @return
+     */
+	public static synchronized String getSerial() { 
+		StringBuffer buffer = new StringBuffer(16);
+		buffer.append(getCurrentTime(8));
+		if (num == 0xffff) num = 1;
+		String str = intToHexString((num++), 4);
+		buffer.append(str);
+		buffer.append(mac);
+        buffer.append(pid);
+        String seq = buffer.toString();
+        return seq;
+	}
+	
 	/**
-	 * 生成xml报文中，message节点的id序列号
+	 * 取当前时间<秒>的16进制表示，时间秒数的全值为int范围内，4个字节，8位16进制表示 （可用22年）
+	 * @param stringLength
 	 * @return
 	 */
-	public static String getSerialId4Message(){
-		// 暂时如此实现
-		return String.valueOf(System.currentTimeMillis());
+	private static String getCurrentTime(int stringLength) {
+		StringBuffer currentTime = new StringBuffer(stringLength);
+		currentTime.append(intToHexString((int) (System.currentTimeMillis() / 1000), stringLength));
+		return currentTime.toString();
+	}
+
+	/**
+	 * 取本机ip的16进制表示，ip全值为4个字节，8位16进制长度
+	 * @param stringLength
+	 * @return
+	 */
+	private static String getAddress(int stringLength) {
+		StringBuffer machineID = new StringBuffer(stringLength);
+		byte addr[] = null;
+		try {
+			addr = InetAddress.getLocalHost().getAddress();
+		} catch (UnknownHostException e) {
+			LOG.error(e.getMessage(), e);
+		}
+		machineID.append(intToHexString(byteToInt(addr), stringLength));
+		return machineID.toString();
+	}
+
+	/**
+	 * 取当前机器的mac地址的md5串；若失败，则取ip16进制表示
+	 * @param len
+	 * @return
+	 */
+	private static String getMachineID(int len) {
+		try {
+			Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+			byte[] hardwareAddress = null;
+			end: while (interfaces.hasMoreElements()) {
+				NetworkInterface inter = interfaces.nextElement();
+				hardwareAddress = inter.getHardwareAddress();
+
+				if (hardwareAddress == null || hardwareAddress.length <= 0) {
+					continue;
+				}
+
+				Enumeration<InetAddress> inetAddressList = inter.getInetAddresses();
+				while (inetAddressList.hasMoreElements()) {
+					InetAddress inetAddress = inetAddressList.nextElement();
+					String hostAddress = inetAddress.getHostAddress();
+					if (!hostAddress.equals("127.0.0.1") && !hostAddress.contains(":") && !hostAddress.contains("%")) {
+						hardwareAddress = NetworkInterface.getByInetAddress(inetAddress).getHardwareAddress();
+						if (hardwareAddress != null && hardwareAddress.length > 0) {
+							break end;
+						}
+					}
+				}
+			}
+			String hex = EncryptUtil.md5(hardwareAddress);
+			return hex.substring(0, len);
+		} catch (Exception e) {
+			LOG.warn(e.getMessage(), e);
+			// 失败，取本机ip
+			return getAddress(len);
+		}
+	}
+
+	/**
+	 * 取当前进程号
+	 * @return
+	 */
+	private static int getProcessId() {
+		try {
+			String name = ManagementFactory.getRuntimeMXBean().getName();
+			int index = name.indexOf("@");
+			return Integer.parseInt(name.substring(0, index));
+		} catch (Exception e) {
+			LOG.warn(e.getMessage(), e);
+		}
+		return new java.util.Random().nextInt();
+	}
+
+	/**
+	 * int转换成指定位数16进制字符串
+	 * @param value int值
+	 * @param stringLength 指定位数
+	 * @return 
+	 */
+	private static String intToHexString(int value, int stringLength) {
+		char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+		StringBuffer buffer = new StringBuffer(stringLength);
+		int shift = stringLength - 1 << 2;
+		for (int i = -1; ++i < stringLength;) {
+			buffer.append(hexDigits[value >> shift & 0XF]);
+			value <<= 4;
+		}
+		return buffer.toString();
+	}
+
+	/**
+	 * byte转换成int
+	 * @param bytes
+	 * @return 
+	 */
+	private static int byteToInt(byte bytes[]) {
+		int value = 0;
+		for (int i = -1; ++i < bytes.length;) {
+			value <<= 8;
+			int b = bytes[i] & 0XFF;
+			value |= b;
+		}
+		return value;
+	}
+	
+	public static void main(String args[]){
+		Set<String> s = new HashSet<String>();
+		for(int i = 0 ; i < 10; i++){
+			String serial = SequenceUtil.getSerial();
+			s.add(serial);
+			LOG.debug("i="+i+", serial="+serial);
+		}
+		LOG.debug("size="+s.size());
 	}
 }
