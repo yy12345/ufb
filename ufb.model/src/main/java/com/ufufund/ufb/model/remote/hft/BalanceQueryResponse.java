b02cdedf3d5ee3b69be16658ac57e94d6a331f37
ayis <nonopo12345@163.com>
2015-04-17 00:24:54 +0800

    份额和交易查询响应list数据jaxb对象化

--- ufb.model/src/main/java/com/ufufund/ufb/model/remote/hft/BalanceQueryResponse.java
+++ ufb.model/src/main/java/com/ufufund/ufb/model/remote/hft/BalanceQueryResponse.java
@@ -1,116 +1,41 @@
 package com.ufufund.ufb.model.remote.hft;
 
+import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 public class BalanceQueryResponse extends AbstractResponse {
 
-	private String TotalRecord;
+	private int TotalRecord;
 
-	//以下字段可能多次出现
-	private String TransactionAccountID;
-	private String FundCode;
-	private String ShareClass;
-	private String TotalFundVol;
-	private String AvailableVol;
-	private String TotalFrozenVol;
-	private String UndistributeMonetaryIncome;
-	private String FundDayIncome;
-	private String TotalIncome;
-	//以上字段可能多次出现
-
-
-	public String getTotalRecord() {
+	@XmlElementWrapper(name="assetList")
+	@XmlElement(name="asset")
+	private List<BalanceQueryAsset> assets;
+	
+	public int getTotalRecord() {
 		return TotalRecord;
 	}
 
-	public void setTotalRecord(String totalRecord) {
+	public void setTotalRecord(int totalRecord) {
 		TotalRecord = totalRecord;
 	}
 
-	public String getTransactionAccountID() {
-		return TransactionAccountID;
+	public List<BalanceQueryAsset> getAssets() {
+		return assets;
 	}
 
-	public void setTransactionAccountID(String transactionAccountID) {
-		TransactionAccountID = transactionAccountID;
-	}
-
-	public String getFundCode() {
-		return FundCode;
-	}
-
-	public void setFundCode(String fundCode) {
-		FundCode = fundCode;
-	}
-
-	public String getShareClass() {
-		return ShareClass;
-	}
-
-	public void setShareClass(String shareClass) {
-		ShareClass = shareClass;
-	}
-
-	public String getTotalFundVol() {
-		return TotalFundVol;
-	}
-
-	public void setTotalFundVol(String totalFundVol) {
-		TotalFundVol = totalFundVol;
-	}
-
-	public String getAvailableVol() {
-		return AvailableVol;
-	}
-
-	public void setAvailableVol(String availableVol) {
-		AvailableVol = availableVol;
-	}
-
-	public String getTotalFrozenVol() {
-		return TotalFrozenVol;
-	}
-
-	public void setTotalFrozenVol(String totalFrozenVol) {
-		TotalFrozenVol = totalFrozenVol;
-	}
-
-	public String getUndistributeMonetaryIncome() {
-		return UndistributeMonetaryIncome;
-	}
-
-	public void setUndistributeMonetaryIncome(String undistributeMonetaryIncome) {
-		UndistributeMonetaryIncome = undistributeMonetaryIncome;
-	}
-
-	public String getFundDayIncome() {
-		return FundDayIncome;
-	}
-
-	public void setFundDayIncome(String fundDayIncome) {
-		FundDayIncome = fundDayIncome;
-	}
-
-	public String getTotalIncome() {
-		return TotalIncome;
-	}
-
-	public void setTotalIncome(String totalIncome) {
-		TotalIncome = totalIncome;
+	public void setAssets(List<BalanceQueryAsset> assets) {
+		this.assets = assets;
 	}
 
 	@Override
 	public String toString() {
-		return "BalanceQueryResponse [TotalRecord=" + TotalRecord
-				+ ", TransactionAccountID=" + TransactionAccountID
-				+ ", FundCode=" + FundCode + ", ShareClass=" + ShareClass
-				+ ", TotalFundVol=" + TotalFundVol + ", AvailableVol="
-				+ AvailableVol + ", TotalFrozenVol=" + TotalFrozenVol
-				+ ", UndistributeMonetaryIncome=" + UndistributeMonetaryIncome
-				+ ", FundDayIncome=" + FundDayIncome + ", TotalIncome="
-				+ TotalIncome + ", toString()=" + super.toString() + "]";
+		return "BalanceQueryResponse [TotalRecord=" + TotalRecord + ", assets="
+				+ assets + ", toString()=" + super.toString() + "]";
 	}
-	
+
 }
--- ufb.remote/src/test/java/com/ufufund/ufb/remote/test/XmlTest.java
+++ ufb.remote/src/test/java/com/ufufund/ufb/remote/test/XmlTest.java
@@ -1,68 +1,23 @@
 package com.ufufund.ufb.remote.test;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import com.ufufund.ufb.common.utils.JaxbUtil;
-import com.ufufund.ufb.model.remote.hft.BankAuthRequest;
+import com.ufufund.ufb.model.remote.hft.BalanceQueryAsset;
+import com.ufufund.ufb.model.remote.hft.BalanceQueryResponse;
 import com.ufufund.ufb.model.remote.hft.BankAuthResponse;
-import com.ufufund.ufb.model.remote.hft.MessageRequest;
 import com.ufufund.ufb.model.remote.hft.MessageResponse;
-import com.ufufund.ufb.model.remote.hft.Requestbody;
 import com.ufufund.ufb.model.remote.hft.Responsebody;
+import com.ufufund.ufb.model.remote.hft.TransQueryAsset;
+import com.ufufund.ufb.model.remote.hft.TransQueryResponse;
 
 @SuppressWarnings("rawtypes")
 public class XmlTest {
 
-//	@Test
-	public void testReqToXml(){
-		
-		BankAuthRequest br = new BankAuthRequest();
-		br.setVersion("1.0.0");
-		br.setMerchantId("000100059");
-		br.setDistributorCode("001002");
-		br.setBusinType("001");
-		br.setApplicationNo("serial11120001222");
-		br.setClearingAgencyCode("0009ccc");
-		
-		Requestbody rb = new Requestbody();
-		rb.setRequest(br);
-		
-		MessageRequest mq = new MessageRequest();
-		mq.setRequestbody(rb);
-		mq.setSignature("asdfsadfsafsafsdfasasdfs");
-		
-		Class[] clazzes = new Class[]{MessageRequest.class, Requestbody.class, BankAuthRequest.class};
-		String xml = JaxbUtil.toXml(mq, clazzes);
-		System.out.println(xml);
-	}
-	
-//	@Test
-	public void testReqToBean(){
-		
-		String xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
-				+"<Message id=\"mq001001002\">                             "
-				+"    <Requestbody>                                      "
-				+"        <Request>"
-				+"            <version>1.0.0</version>                 "
-				+"            <merchantId>000100059</merchantId>       "
-				+"            <distributorCode>001002</distributorCode>"
-				+"            <businType>001</businType>                      "
-				+"            <applicationNo>serial11120001222</applicationNo>"
-				+"            <clearingAgencyCode>0009ccc</clearingAgencyCode>"
-				+"        </Request>                                 "
-				+"    </Requestbody>                                 "
-				+"    <Signature>asdfsadfsafsafsdfasasdfs</Signature>"
-				+"</Message>";
-		
-		xml = JaxbUtil.buildRequestXml(xml, BankAuthRequest.class);
-		System.out.println(xml);
-		
-		Class[] clazzes = new Class[]{MessageRequest.class, Requestbody.class, BankAuthRequest.class};
-		MessageRequest mq = JaxbUtil.toBean(xml, clazzes);
-		
-		System.out.println(mq);
-	}
-	
 //	@Test
 	public void testResToXml(){
 		
@@ -89,7 +44,7 @@ public class XmlTest {
 		System.out.println(xml);
 	}
 	
-	@Test
+//	@Test
 	public void testResToBean(){
 		
 //		String xml = "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>"
@@ -122,10 +77,133 @@ public class XmlTest {
 	}
 	
 //	@Test
-	public void TestGetBoundTypeName(){
+	public void testList2Xml(){
 		
-		String boundType = JaxbUtil.getBoundTypeName(BankAuthResponse.class);
-		System.out.println("boundType="+boundType);
+		List<TransQueryAsset> assets = new ArrayList<TransQueryAsset>();
+		TransQueryAsset asset1 = new TransQueryAsset();
+		asset1.setAppSheetSerialNo("sheet-001");
+		TransQueryAsset asset2 = new TransQueryAsset();
+		asset2.setAppSheetSerialNo("sheet-002");
+		TransQueryAsset asset3 = new TransQueryAsset();
+		asset3.setAppSheetSerialNo("sheet-003");
+		assets.add(asset1);
+		assets.add(asset2);
+		assets.add(asset3);
+		
+		TransQueryResponse tqs = new TransQueryResponse();
+		tqs.setVersion("1.0.0");
+		tqs.setApplicationNo("xxx-0001");
+		tqs.setTotalRecord(assets.size());
+		tqs.setAssets(assets);
+		
+		Responsebody rb = new Responsebody();
+		rb.setResponse(tqs);
+		
+		MessageResponse mr = new MessageResponse();
+		mr.setResponsebody(rb);
+		mr.setSignature("ssssssss----sssss");
+		
+		Class[] clazzes = new Class[]{MessageResponse.class, Responsebody.class, TransQueryResponse.class};
+		String xml = JaxbUtil.toXml(mr, clazzes);
+		System.out.println(xml);
 	}
+	
+//	@Test
+	public void testXml2List(){
+		
+		String xml = "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>   "
+				+"<Message>                                                                                             "
+				+"    <Responsebody>                                                                                    "
+				+"        <Response xsi:type=\"transQueryResponse\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"
+				+"            <Version>1.0.0</Version>                                                                  "
+				+"            <ApplicationNo>xxx-0001</ApplicationNo>                                                   "
+				+"            <TotalRecord>3</TotalRecord>                                                              "
+				+"            <assetList>                                                                               "
+				+"                <asset>                                                                               "
+				+"                    <AppSheetSerialNo>sheet-001</AppSheetSerialNo>                                    "
+				+"                </asset>                                                                              "
+				+"                <asset>                                                                               "
+				+"                    <AppSheetSerialNo>sheet-002</AppSheetSerialNo>                                    "
+				+"                </asset>                                                                              "
+				+"                <asset>                                                                               "
+				+"                    <AppSheetSerialNo>sheet-003</AppSheetSerialNo>                                    "
+				+"                </asset>                                                                              "
+				+"            </assetList>                                                                              "
+				+"        </Response>                                                                                   "
+				+"    </Responsebody>                                                                                   "
+				+"    <Signature>ssssssss----sssss</Signature>                                                          "
+				+"</Message>                                                                                            ";
+		
+		Class[] clazzes = new Class[]{MessageResponse.class, Responsebody.class, TransQueryResponse.class};
+		MessageResponse mq = JaxbUtil.toBean(xml, clazzes);
+		
+		System.out.println(mq);
+	}
+	
+//	@Test
+	public void testList2Xml2(){
+		
+		List<BalanceQueryAsset> assets = new ArrayList<BalanceQueryAsset>();
+		BalanceQueryAsset asset1 = new BalanceQueryAsset();
+		asset1.setAvailableVol(new BigDecimal("100.12345"));
+		BalanceQueryAsset asset2 = new BalanceQueryAsset();
+		asset2.setAvailableVol(new BigDecimal("200.12345"));
+		BalanceQueryAsset asset3 = new BalanceQueryAsset();
+		asset3.setAvailableVol(new BigDecimal("300.12345"));
+		assets.add(asset1);
+		assets.add(asset2);
+		assets.add(asset3);
+		
+		BalanceQueryResponse bqs = new BalanceQueryResponse();
+		bqs.setVersion("1.0.0");
+		bqs.setApplicationNo("xxx-0001");
+		bqs.setTotalRecord(assets.size());
+		bqs.setAssets(assets);
+		
+		Responsebody rb = new Responsebody();
+		rb.setResponse(bqs);
+		
+		MessageResponse mr = new MessageResponse();
+		mr.setResponsebody(rb);
+		mr.setSignature("ssssssss----sssss");
+		
+		Class[] clazzes = new Class[]{MessageResponse.class, Responsebody.class, BalanceQueryResponse.class};
+		String xml = JaxbUtil.toXml(mr, clazzes);
+		System.out.println(xml);
+	}
+	
+	@Test
+	public void testXml2List2(){
+		
+		String xml = "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>                                                     "
+				+"<Message>                                                                                                "
+				+"    <Responsebody>                                                                                       "
+				+"        <Response xsi:type=\"balanceQueryResponse\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"> "
+				+"            <Version>1.0.0</Version>                                                                     "
+				+"            <ApplicationNo>xxx-0001</ApplicationNo>                                                      "
+				+"            <TotalRecord>3</TotalRecord>                                                                 "
+				+"            <assetList>                                                                                  "
+				+"                <asset>                                                                                  "
+				+"                    <AvailableVol>100.12345</AvailableVol>                                               "
+				+"                </asset>                                                                                 "
+				+"                <asset>                                                                                  "
+				+"                    <AvailableVol>200.12345</AvailableVol>                                               "
+				+"                </asset>                                                                                 "
+				+"                <asset>                                                                                  "
+				+"                    <AvailableVol>300.12345</AvailableVol>                                               "
+				+"                </asset>                                                                                 "
+				+"            </assetList>                                                                                 "
+				+"        </Response>                                                                                      "
+				+"    </Responsebody>                                                                                      "
+				+"    <Signature>ssssssss----sssss</Signature>                                                             "
+				+"</Message>                                                                                               ";
+	
+		Class[] clazzes = new Class[]{MessageResponse.class, Responsebody.class, BalanceQueryResponse.class};
+		MessageResponse mq = JaxbUtil.toBean(xml, clazzes);
+		
+		System.out.println(mq);
+	}
+	
+	
 	
 }
