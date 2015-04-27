6bcaa30cee202b5a49bdb9ca81bdf0cfdddcb5ef
luanchengwei <luan_chengwei@pactera.com>
2015-04-27 10:28:27 +0800

    js 修复

--- ufb.web/src/main/webapp/js/scripts/register.js
+++ ufb.web/src/main/webapp/js/scripts/register.js
@@ -15,21 +15,38 @@ function isSameWord(str) {
     }
     return true
 }
+function trim(a) {
+    return a.replace(/(^\s*)|(\s*$)/g, "")
+}
 
+function ltrim(a) {
+    return a.replace(/(^\s*)/g, "")
+}
+
+function rtrim(a) {
+    return a.replace(/(\s*$)/g, "")
+}
+
+function isNull(a) {
+    return a == null || a == undefined || $.trim(a).length == 0
+}
+
+function isNotNull(a) {
+    return !isNull(a)
+}
 /* error mod */
-function showErrorInfo(wrap, errText) {
+var showErrorInfo = function (wrap, errText) {
     wrap.find(".tips-check").html("<i class='icon'></i>" + errText);
     wrap.removeClass("ok");
     wrap.addClass("error");
 }
-function showOkInfo(wrap) {
+var showOkInfo = function (wrap) {
     wrap.find(".tips-check").html("<i class='icon'></i>");
     wrap.removeClass("error");
     wrap.addClass("ok");
 }
-function showTips(ele, text) {
+var showTips = function (ele, text) {
     var _wrap = ele.parents("li");
-    var _tips = _wrap.find(".tips-check");
     if(!text) {
         showOkInfo(_wrap);
     }else {
@@ -38,219 +55,171 @@ function showTips(ele, text) {
 }
 
 /* check mod */
-var check_mobile = function (ipt) {
+var check_mobile = function (ipt) {//验证手机号
     var value = ipt.val();
-    if (value == "" || value == ipt.context.defaultValue) {
-        return 1
+    if (isNull(value)) {
+        return showTips(ipt, "手机号不能为空");
     }
     var regex = /^1\d{10}$/;
     if (!regex.test(value)) {
-        return 2
+        return showTips(ipt, "格式错误，请输入正确的手机号码");
     }
-    return 0
+    return showTips(ipt, 0);
 }
-var check_code = function (ipt) {
+var check_code = function (ipt) {//验证验证码
     var value = ipt.val();
-    if (value == "" || value == ipt.context.defaultValue) {
-        return 1
+    if (isNull(value)) {
+        return showTips(ipt, "验证码不能为空");
     }
     if (value.length != 4) {
-        return 2
+        return showTips(ipt, "验证码为4位数字");
     }
-    return 0
+    return showTips(ipt, 0);
 }
-var check_phoneCode = function (ipt) {
+var check_phoneCode = function (ipt) {//验证验证码
     var value = ipt.val();
-    if (value == "" || value == ipt.context.defaultValue) {
-        return 1
+    if (isNull(value)) {
+        return showTips(ipt, "手机验证码不能为空");
     }
     if (value.length != 6) {
-        return 2
+        return showTips(ipt, "验证码为6位数字");
     }
-    return 0
+    return showTips(ipt, 0);
 }
-var check_pwd1 = function (ipt) {
+
+var check_id = function (ipt) {//验证身份证号
+    var value = ipt.val();
+    if (isNull(value)) {
+        return showTips(ipt, "身份证不能为空");
+    }
+    var regex = /^((11|12|13|14|15|21|22|23|31|32|33|34|35|36|37|41|42|43|44|45|46|50|51|52|53|54|61|62|63|64|65|71|81|82|91)\d{4})((((19|20)(([02468][048])|([13579][26]))0229))|((20[0-9][0-9])|(19[0-9][0-9]))((((0[1-9])|(1[0-2]))((0[1-9])|(1\d)|(2[0-8])))|((((0[1,3-9])|(1[0-2]))(29|30))|(((0[13578])|(1[02]))31))))((\d{3}(x|X))|(\d{4}))$/;
+    if (!regex.test(value)) {
+        return showTips(ipt, "格式错误，请输入正确的身份证号");
+    }
+    return showTips(ipt, 0);
+}
+var check_pwd1 = function (ipt) {//验证密码
     var value = ipt.val();
     if (value == "") {
-        return 1
+        return showTips(ipt, "密码不能为空");
     }
-    if (value.length > 20) {
-        return 2
-    }
-    if (value.length < 6) {
-        return 3
+    if (value.length < 6 || value.length > 20) {
+        return showTips(ipt, "密码为6-20位字符");
     }
     var regex1 = /\s+/;
     if (regex1.test(value)) {
-        return 4
+        return showTips(ipt, "密码中不允许有空格");
     }
     var regex2 = /^[0-9]+$/;
     if (regex2.test(value)) {
-        return 5
+        return showTips(ipt, "密码不能全为数字");
     }
     var regex3 = /^[a-zA-Z]+$/;
     if (regex3.test(value)) {
-        return 6
+        return showTips(ipt, "密码不能全为字母，请包含至少1个数字或符号");
     }
     var regex4 = /^[^0-9A-Za-z]+$/;
     if (regex4.test(value)) {
-        return 7
+        return showTips(ipt, "密码不能全为符号");
     }
     if (isSameWord(value)) {
-        return 8
+        return showTips(ipt, "密码不能全为相同字符或数字");
     }
-    var n = "d*" + commonSymbol + "";
-    var regexAll = "\\\d+[A-Za-z]|[A-Za-z]+[0-9]+|[A-Za-z]+" + commonSymbol + "[0-9]+|[A-Za-z]+[0-9]+" + commonSymbol + "|" + n + "";
+    var regexAll = "^([0-9]+|[a-zA-Z]+|" + commonSymbol + ")+$";
     var regex5 = new RegExp(regexAll);
     if (!regex5.test(value)) {
-        return 10
+        return showTips(ipt, "密码不符合规范，必须包含数字，字母，特殊字符中的两者或三者");
     }
-    return 0
+    return showTips(ipt, 0);
 }
-function check_pwd2(ipt) {
+ var check_pwd2 = function (ipt) {//验证重复密码
     var _form = ipt.parents("form");
 
     var _valuePassword1 = _form.find(".ipt-password1").val();
     var _valuePassword2 = ipt.val();
     if (_valuePassword2 == "") {
-        return 1
+        return showTips(ipt, "确认密码不能为空");
     }
     if (_valuePassword1 != _valuePassword2) {
-        return 2
+        return showTips(ipt, "两次密码输入不一致");
     }
-    return 0
+    return showTips(ipt, 0);
 }
-var check_organization = function (ipt) {
-    var value = ipt.val();
-    if (value == "" || value == ipt.context.defaultValue) {
-        return 1
-    }
-    return 0
+ 
+ var check_mobile = function (ipt) {//验证手机号
+	    var value = ipt.val();
+	    if (isNull(value)) {
+	        return showTips(ipt, "手机号不能为空");
+	    }
+	    var regex = /^1\d{10}$/;
+	    if (!regex.test(value)) {
+	        return showTips(ipt, "格式错误，请输入正确的手机号码");
+	    }
+	    return showTips(ipt, 0);
+	}
+ 
+var check_organization = function (ipt) {//幼教机构名称
+	var value = ipt.val();
+	if (isNull(value)) {
+		return showTips(ipt, "幼教机构名称不能为空");
+	}
+	return showTips(ipt, 0)
 }
-var check_business = function (ipt) {
+ 
+var check_business = function (ipt) {//营业执照注册号
+	var value = ipt.val();
+	if (isNull(value)) {
+		return showTips(ipt, "营业执照注册号不能为空");
+	}
+	return showTips(ipt, 0)
+}
+
+var check_bankCard = function (ipt) {//验证银行卡
     var value = ipt.val();
-    if (value == "" || value == ipt.context.defaultValue) {
-        return 1
+    if (isNull(value)) {
+        return showTips(ipt, "银行卡不能为空");
     }
-    return 0
+    var regex = /^\d{12,19}$/;
+    if (!regex.test($.trim(value))) {
+        return showTips(ipt, "银行卡格式错误");
+    }
+    return showTips(ipt, 0)
 }
 /* bind mod */
 
 $(".ipt-phoneNum").bind("blur", function() {
-    var _this = $(this);
-    var checkCode = check_mobile(_this);
-    switch (checkCode) {
-        case 1:
-            showTips(_this, "手机号不能为空");
-            break;
-        case 2:
-            showTips(_this, "格式错误，请输入正确的手机号码");
-            break;
-        case 0:
-            showTips(_this, 0);
-    }
+    check_mobile($(this));
 });
 
 $(".ipt-code").bind("blur", function() {
-    var _this = $(this);
-    var checkCode = check_code(_this);
-    switch (checkCode) {
-        case 1:
-            showTips(_this, "验证码不能为空");
-            break;
-        case 2:
-            showTips(_this, "验证码为4位数字");
-            break;
-        case 0:
-            showTips(_this, 0);
-    }
+    check_code($(this));
 });
 
 $(".ipt-phoneCode").bind("blur", function() {
-    var _this = $(this);
-    var checkCode = check_phoneCode(_this);
-    switch (checkCode) {
-        case 1:
-            showTips(_this, "手机验证码不能为空");
-            break;
-        case 2:
-            showTips(_this, "验证码为6位数字");
-            break;
-        case 0:
-            showTips(_this, 0);
-    }
+    check_phoneCode($(this));
+});
+
+$(".ipt-idCard").bind("blur", function() {
+    check_id($(this));
 });
 
 $(".ipt-password1").on("blur", function() {
-    var _this = $(this);
-    var checkCode = check_pwd1(_this);
-    switch (checkCode) {
-        case 1:
-            showTips(_this, "密码不能为空");
-            break;
-        case 2:
-            showTips(_this, "密码为6-20位字符");
-            break;
-        case 3:
-            showTips(_this, "密码为6-20位字符");
-            break;
-        case 4:
-            showTips(_this, "密码中不允许有空格");
-            break;
-        case 5:
-            showTips(_this, "密码不能全为数字");
-            break;
-        case 6:
-            showTips(_this, "密码不能全为字母，请包含至少1个数字或符号 ");
-            break;
-        case 7:
-            showTips(_this, "密码不能全为符号");
-            break;
-        case 8:
-            showTips(_this, "密码不能全为相同字符或数字");
-            break;
-        case 0:
-            showTips(_this, 0);
-    }
+    check_pwd1($(this));
 });
-
 
 $(".ipt-password2").on("blur", function() {
-    var _this = $(this);
-    var checkCode = check_pwd2(_this);
-    switch (checkCode) {
-        case 1:
-            showTips(_this, "确认密码不能为空");
-            break;
-        case 2:
-            showTips(_this, "两次密码输入不一致");
-            break;
-        case 0:
-            showTips(_this, 0);
-    }
+    check_pwd2($(this));
 });
 
-$(".ipt-organization").bind("blur", function() {
-    var _this = $(this);
-    var checkCode = check_organization(_this);
-    switch (checkCode) {
-        case 1:
-            showTips(_this, "幼教机构名称不能为空");
-            break;
-        case 0:
-            showTips(_this, 0);
-    }
+$(".ipt-organization").on("blur", function() {
+	check_organization($(this));
 });
 
-$(".ipt-business").bind("blur", function() {
-    var _this = $(this);
-    var checkCode = check_business(_this);
-    switch (checkCode) {
-        case 1:
-            showTips(_this, "营业执照注册号不能为空");
-            break;
-        case 0:
-            showTips(_this, 0);
-    }
+$(".ipt-business").on("blur", function() {
+	check_business($(this));
+});
+
+$(".ipt-bankCard").on("blur", function() {
+    check_bankCard($(this));
 });
 
